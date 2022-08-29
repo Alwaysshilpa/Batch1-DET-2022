16a02777396f936bf03c2f8a7a5425fad514f2a9
shilpa <shilpasekhar20202@gmail.com>
2022-08-29 18:20:05 +0530

    Modified

---
+++ Batch1-DET-2022/Age.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Batch1_DET_2022
+{
+    public class Age
+    {
+        public int age { get; set; }
+        public string Print()
+        {
+            return $"Age={age}";
+        }
+    }
+}
---
+++ Batch1-DET-2022/Classmain1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Batch1_DET_2022
+{
+    
+    
+        public class Classmain1
+        {
+            public static Student Student { get; private set; }
+
+            public static void Main()
+            {
+                ScienceStudent Student = new ScienceStudent { regno =10, Name = "shilpa" };
+                ScienceStudent sciencesubject = new ScienceStudent { physics = 75, chemistry = 65, maths = 45};
+                CommerceStudent Commercessubject = new CommerceStudent { economices = 74, accounts = 85, banking = 75 };
+
+                Console.WriteLine(sciencesubject.GetAvgMarks());
+                Console.WriteLine(Commercessubject.GetAvgMarks());
+            }
+        }
+    }
+
+
+
