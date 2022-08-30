6b73c0c5c181d79132624bfc3ed7020a415e1b23
shilpa <shilpasekhar20202@gmail.com>
2022-08-30 18:09:12 +0530

    New Commit

---
+++ Batch1-DET-2022/CSFeatures.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Batch1_DET_2022
+{
+    internal class CSFeatures
+    {
+        public static void Main()
+        {
+            Action<string> action = (string name) => { Console.WriteLine($"Hai {name}"); };
+            Action<string, string> action1 = (string s1, string s2) => { Console.WriteLine($"Hai {s1}, " + $"you said {s2}"); };
+            action.Invoke("shilpa");
+            action1.Invoke("shilpa", "am in bglr");
+
+            Action<int, string, string> printEmp = (int id, string name, string email) =>
+            {
+                Console.WriteLine($"id={id}, name={name}, email={email}");
+            };
+            printEmp.Invoke(5050, "shilpa", "shilpa.com");
+        }
+    }
+}
+
+   
---
+++ Batch1-DET-2022/Icar.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Batch1_DET_2022
+{
+    internal interface Icar
+    {
+        int GetPrice(string name);
+
+    }
+    class BMW : Icar
+    {
+        public int GetPrice(string name)
+        {
+            if (name == "M3")
+                return 1300000;
+            else if (name == "X7")
+                return 9600000;
+            else
+                return 1000000;
+        }
+    }
+}
